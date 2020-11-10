f50d4ff7dca922e883e670122b020fb1ae6ae89b
Crispan Gari <crispengari@gmail.com>
2020-11-10 10:54:44 +0200

    crisp-album-rn-expo-mobile-app

---
+++ Components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {View, Text, TouchableOpacity} from 'react-native'
+import styles from './HeaderStyles'
+import { AntDesign, Ionicons} from '@expo/vector-icons';
+const Header = ({setAlbumAssets, albumAssets, setImage}) => {
+    const choseImage =()=>{
+        setAlbumAssets(null)
+        setImage(null)
+    }
+    return (
+        <View style={styles.header}>
+            <TouchableOpacity style={styles.header__camera} onPress={choseImage}>
+            {
+                !albumAssets?<AntDesign name="picture" size={24} color="orange" /> :<Ionicons name="md-arrow-round-back" size={24} color="orange" />
+            }
+            </TouchableOpacity>
+            <Text style={styles.header__text}>
+                Crisp Albums
+            </Text>
+            <TouchableOpacity style={styles.header__share} onPress={openAndShare}>
+            <AntDesign name="setting" size={24} color="orange" />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default Header
